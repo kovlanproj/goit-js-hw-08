f7eb075ae7359a301b10abd420e3a892cb0918f6
kovlanproj <kozorizva@gmail.com>
2022-07-03 01:54:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+
+const formData = {};
+const FORM_DATA = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+dataRecovery();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA);
+}
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+  localStorage.setItem(FORM_DATA, JSON.stringify(formData));
+}
+
+function dataRecovery() {
+  const data = JSON.parse(localStorage.getItem(FORM_DATA));
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+}
